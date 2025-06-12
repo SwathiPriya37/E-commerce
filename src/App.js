9eb96c963fe3c230f427e680188c381152ca064a
SwathiPriya37 <rswathipriya3@gmail.com>
2025-06-12 19:46:13 +0530

    modified the whole file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './components/Pages/Shop';
+import ShopCategory from './components/Pages/ShopCategory';
+import LoginSignup from './components/Pages/LoginSignup';
+import Cart from './components/Pages/Cart';
+import Product from './components/Pages/Product'; // ✅ FIXED
 
 function App() {
   return (
-    <div >
-      <Navbar />
-      </div>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/mens' element={<ShopCategory category="men" />} />
+          <Route path='/womens' element={<ShopCategory category="women" />} />
+          <Route path='/kids' element={<ShopCategory category="kids" />} />
+          <Route path='/product/:productId' element={<Product />} /> {/* ✅ FIXED */}
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
