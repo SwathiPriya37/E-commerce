8c00917b5396ea1b15a2872812193ffefb803b14
SwathiPriya37 <rswathipriya3@gmail.com>
2025-06-13 20:20:21 +0530

    updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
 
@@ -15,16 +16,16 @@ const Navbar = () => {
       </div>
       <ul className="nav-menu">
         <li onClick={() => setMenu("shop")}>
-          <Link to="/">Shop</Link>{menu === "shop" ? <hr /> : <></>}
+          <Link style={{textDecoration: 'none'}}to="/">Shop</Link>{menu === "shop" ? <hr /> : <></>}
         </li>
         <li onClick={() => setMenu("mens")}>
-          <Link to="/mens">Men</Link>{menu === "mens" ? <hr /> : <></>}
+          <Link style={{textDecoration: 'none'}}to="/mens">Men</Link>{menu === "mens" ? <hr /> : <></>}
         </li>
         <li onClick={() => setMenu("womens")}>
-          <Link to="/womens">Women</Link>{menu === "womens" ? <hr /> : <></>}
+          <Link style={{textDecoration: 'none'}}to="/womens">Women</Link>{menu === "womens" ? <hr /> : <></>}
         </li>
         <li onClick={() => setMenu("kids")}>
-          <Link to="/kids">Kids</Link>{menu === "kids" ? <hr /> : <></>}
+          <Link style={{textDecoration: 'none'}}to="/kids">Kids</Link>{menu === "kids" ? <hr /> : <></>}
         </li>
       </ul>
       <div className="nav-login-cart">
