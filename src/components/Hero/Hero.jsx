d5e7685722db75282019b9b37db4ad33ee7ddad4
SwathiPriya37 <rswathipriya3@gmail.com>
2025-06-13 20:19:05 +0530

    updated file

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import './Hero.css';
-import hand_icon from '../Assets/hand_icon.png';
-import arrow_icon from '../Assets/arrow.png';
-import hero_image from '../Assets/hero_image.png';
+import React from "react";
+import "./Hero.css";
+import hand_icon from "../Assets/hand_icon.png";
+import arrow_icon from "../Assets/arrow.png";
+import hero_image from "../Assets/hero_image.png";
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div className="hero">
       <div className="hero-left">
-        <h2>NEW ARIVALS ONLY</h2>
+        <h2>NEW ARRIVALS ONLY</h2>
         <div className="hand-hand-icon">
           <p>new</p>
           <img src={hand_icon} alt="" />
         </div>
-        <p>collections</p>
-        <p>for everyone</p>
+        <p>Collections</p>
+        <p>for everyone </p>
+        <div className="hero-latest-btn">
+          <div>Latest Collection</div>
+          <img src={arrow_icon} alt="arrow" />
+        </div>
       </div>
-      <div className="her-latest-btn">
-        <div>Latest Collection</div>
-        <img src={arrow_icon} alt="" />
-      </div>
-      <div className="hero-right">
-        <img src={hero_image} alt="" />
+      <div className="hero_right">
+        <img src={hero_image} alt="hero_image" />
       </div>
     </div>
   );
