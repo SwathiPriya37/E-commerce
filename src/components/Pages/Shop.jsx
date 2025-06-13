218944188bf00c55fafc9949a3dd638f8cddbdef
SwathiPriya37 <rswathipriya3@gmail.com>
2025-06-13 19:25:04 +0530

    updated popular, offers path

--- src/components/Pages/Shop.jsx
+++ src/components/Pages/Shop.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Hero from '../Hero/Hero';
+import Popular from '../Popular/Popular';
+import Offers from '../Offers/Offers';
+
 
 const Shop = () => {
   return (
     <div>
-      
+      <Hero/>
+      <Popular/>
+      <Offers/>
     </div>
   )
 }
